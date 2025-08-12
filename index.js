882b2f1f99a59534a24f51e1412567ddff1b5cf7
Saad05-dev <bougrasaad@gmail.com>
2025-08-12 21:53:19 +0100

    Adding eventListener

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 // Variables
-let textInputElement = document.getElementById("#text-input");
-let checkButton = document.getElementById("#check-btn");
-let result = document.getElementById("#result");
+let textInputElement = document.getElementById("text-input");
+let checkButton = document.getElementById("check-btn");
+let result = document.getElementById("result");
 //let palindrome = false; //to check if a word is a palindrome
 // Check input is valid
 function isValidInput(input)
 {
-    if(input === null)
+    if(!input)
     {
-        //alert("Please input a value");
+        alert("Please input a value");
         return false;
     }
-    else if(input.length === 1)
-    {
-        return true;
-    }
+    return true;
 }
-// get input value
-function getInput(input)
-{
-    if(isValidInput(input))
-    {
-        let textInput = textInputElement.value;
-    }
-    else
-    {
-        return false;
-    }
-}
-// Button event listener
 // check if it is palindrome
 function isPalindrome(input)
 {
-    
+    return input === input.split("").reverse().join("");
 }
-// update result
+// Button event listener
+checkButton.addEventListener("click", function (){
+    let textInput = textInputElement.value.trim();
+
+    if(isValidInput(textInput))
+    {
+        if(isPalindrome(textInput))
+        {
+            result.textContent = `"${textInput}" is a palindrome.`;
+        }
+        else
+        {
+            result.textContent = `"${textInput}" is not a palindrome.`;
+        }
+    }
+});
